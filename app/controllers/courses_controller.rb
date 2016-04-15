0e316fa9cc3462fdce73f2381affc93b5f69b770
Diego Munera <dmunera@socobox.co>
2016-04-15 08:11:11 -0500

    Lesson 4 of 4 Heroku deployment

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,6 +1,7 @@
 class CoursesController < ApplicationController
   def index
   	@search_term = params[:looking_for] || 'jhu'
+  	@search_term ||= 'jhu'
   	@courses = Coursera.for @search_term
   end
 end
